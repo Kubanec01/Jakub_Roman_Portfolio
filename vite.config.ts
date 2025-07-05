60cbbdf94cf3d3969a8b3004201abf620518a8a1
Jakub Roman <jakub.m.roman@gmail.com>
2025-07-05 19:28:52 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Jakub_Roman_Portfolio/",
   plugins: [react(), tailwindcss()],
 });
